c9051dc3a302a37a51551f593cc7eeef3a0d65ad
Erik L. Arneson <earneson@arnesonium.com>
2021-01-21 12:22:01 -0800

    Cleaning up some broken code
    This works outside vterm. Haven't tested within it yet.

--- conf.d/vfish.fish
+++ conf.d/vfish.fish
@@ -1,25 +1,12 @@
 # VTerm (https://github.com/akermu/emacs-libvterm#shell-side-configuration)
 function _vfish_install --on-event vfish_install
-    if [ "vterm" == $INSIDE_EMACS ]
-        function vterm_printf;
-            if [ -n "$TMUX" ]
-                # tell tmux to pass the escape sequences through
-                # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-                printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
-            else if string match -q -- "screen*" "$TERM"
-                # GNU screen (screen, screen-256color, screen-256color-bce)
-                printf "\eP\e]%s\007\e\\" "$argv"
-            else
-                printf "\e]%s\e\\" "$argv"
-            end
-        end
-
+    if test "vterm" = "$INSIDE_EMACS"
         function vterm_prompt_end;
             vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
         end
 
         # vterm does not work with tide, unfortunately
-        if function --query tide
+        if functions --query tide
             function fish_prompt -d "Write out the prompt"
                 printf '%s@%s%s%s%s> ' (whoami) (hostname | cut -d . -f 1) \
                     (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
@@ -45,11 +32,5 @@ function _vfish_install --on-event vfish_install
             echo ":"
             pwd
         end
-    end
-end
-
-function _vfish_uninstall --on-event vfish_uninstall
-    if functions --query vterm_old_fish_prompt
-        functions --copy vterm_old_fish_prompt fish_prompt
     end
 end
